f01c7d33e981474af26acbc4ff9d463a0b6ce040
André Königer <andre.koeniger1997@gmail.com>
2023-05-12 07:38:41 +0200

    add test, add coveralls badge

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 [![Scala CI](https://github.com/andre-koe/another-scala-game/actions/workflows/scala.yml/badge.svg?branch=development)](https://github.com/andre-koe/another-scala-game/actions/workflows/scala.yml)
 [![codecov](https://codecov.io/gh/andre-koe/another-scala-game/branch/development/graph/badge.svg?token=ZBBnZJAtI2)](https://codecov.io/gh/andre-koe/another-scala-game)
+[![Coverage Status](https://coveralls.io/repos/github/andre-koe/another-scala-game/badge.svg)](https://coveralls.io/github/andre-koe/another-scala-game)
--- src/main/scala/model/utils/GameObjectUtils.scala
+++ src/main/scala/model/utils/GameObjectUtils.scala
@@ -11,14 +11,13 @@ class GameObjectUtils():
     val technology = findInTech(str)
     val building = findInBuildings(str)
     val unit = findInUnits(str)
-
     technology.orElse(building).orElse(unit).orElse(None)
     
-  def findInTech(str: String): Option[ITechnology] = existsInFactory(str, () => TechnologyFactory())
+  private def findInTech(str: String): Option[ITechnology] = existsInFactory(str, () => TechnologyFactory())
 
-  def findInBuildings(str: String): Option[IBuilding] = existsInFactory(str, () => BuildingFactory())
+  private def findInBuildings(str: String): Option[IBuilding] = existsInFactory(str, () => BuildingFactory())
     
-  def findInUnits(str: String): Option[IUnit] = existsInFactory(str, () => UnitFactory())
+  private def findInUnits(str: String): Option[IUnit] = existsInFactory(str, () => UnitFactory())
 
   private def existsInFactory[T <: IGameObject](name: String, factory: () => IPurchasableFactory[T]): Option[T] =
     factory().create(name.toLowerCase)

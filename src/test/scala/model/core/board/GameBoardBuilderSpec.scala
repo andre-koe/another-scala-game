b5798e1fa0c28470858f035cbf5ef78dd1443f0d
andre-koe <66854219+andre-koe@users.noreply.github.com>
2023-06-27 20:41:55 +0200

    Fix fleet manager bug (#16)
    * first draft
    
    * Major refactorings some tests failing
    
    * fix errors, fix tests
    
    * fix build failure
    
    * fix build failure
    
    * test refactorings
    
    * Major refactorings part xviii
    
    * refactor controller
    
    * gui refactorings
    
    * gui refactorings
    
    * fix bug where fleet creation in sector passes wrong affiliation (indirectly)
    
    * fix tests
    
    * fix tests
    
    * fix tests

--- src/test/scala/model/core/board/GameBoardBuilderSpec.scala
+++ src/test/scala/model/core/board/GameBoardBuilderSpec.scala
@@ -1,5 +1,37 @@
 package model.core.board
 
-class GameBoardBuilderSpec {
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
 
+class GameBoardBuilderSpec extends AnyWordSpec with Matchers {
+
+  "A GameBoardBuilder" should {
+
+    "correctly set cols with withSizeX" in {
+      val builder = GameBoardBuilder()
+      val newBuilder = builder.withSizeX(10)
+      newBuilder.cols should be (10)
+    }
+
+    "correctly set rows with withSizeY" in {
+      val builder: IGameBoardBuilder = GameBoardBuilder()
+      val newBuilder = builder.withSizeY(10)
+      newBuilder.rows should be (10)
+    }
+
+    "build a GameBoard with correct rows and cols" in {
+      val builder: IGameBoardBuilder = GameBoardBuilder()
+      val gameBoard = builder.build
+      gameBoard.sizeX should be (7)
+      gameBoard.sizeY should be (7)
+    }
+
+    "correctly set data with withData" in {
+      val builder: IGameBoardBuilder = GameBoardBuilder().withSizeX(10).withSizeY(10)
+      val newBuilder = builder.withData
+      newBuilder.data.size should be(10)
+      newBuilder.data.foreach(row => row.size should be(10))
+    }
+
+  }
 }
